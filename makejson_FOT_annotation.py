b7f171b7573c98f2700af3a4f3f3a6035696e709
Y.Hooon.J <riverwind97@ajou.ac.kr>
2023-05-18 01:21:04 +0900

    "GPS, 개선, "

--- makejson_FOT_annotation.py
+++ makejson_FOT_annotation.py
@@ -147,11 +147,12 @@ class MakeJson():
             if self.type == "CN7":
                 mat = mat73.loadmat(matList[num])
                 mat = h5py.File(matList[num])
-                
-            FRAMESIZE,CHASSIS_STATUS = self.GetVehicleFrameSize(self.type, mat)
+            
+            FRAMESIZE = matSf['SF_PP']['sim_time'][0,0].shape[0]
+            # FRAMESIZE,CHASSIS_STATUS = self.GetVehicleFrameSize(self.type, mat)
             try:
                 registrationFileRoad = pd.read_excel(self.rgDir +r'\Raw\Registration_' + self.type + r'_' + self.date + r'.xlsx',sheet_name = "road")
-                registrationFileWrong = pd.read_excel(self.rgDir +r'\Raw\Registration_' + self.type + r'_' + self.date + r'.xlsx',sheet_name = "road")
+                registrationFileWrong = pd.read_excel(self.rgDir +r'\Raw\Registration_' + self.type + r'_' + self.date + r'.xlsx',sheet_name = "wrong")
             except:
                 raise Exception("Registration.xlsx가 없습니다.")
             
@@ -453,8 +454,344 @@ class MakeJson():
             if file.endswith('_AutoCuration_temp.json'):
                 os.remove(file)
         print("########## CODE " +self.type + "_" + "AUTOCURATION END   ##########")
+    
+    def CheckType(self):
+        if self.matDir.split('\\\\192.168.75.251\\Shares')[1][5] == 'G':
+            return 'RG3'
+        elif self.matDir.split('\\\\192.168.75.251\\Shares')[1][5] == 'A':
+            return 'CN7'
+        else:
+            raise Exception("부적절한 경로 입력입니다.")
 
+    def MakeJsonPath(self, dir , name):
+        if os.path.isdir(dir) == False: 
+            os.mkdir(dir)
+            print([name + " path generated!"])
+    
+    def ConverSize(self, size_bytes):
+        import math
+        """_summary_
+        Args:
+            size_bytes (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if size_bytes == 0:
+            return "0B"
+
+        # size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = int(math.floor(math.log(size_bytes, 1000)))
+        
+        p = math.pow(1024, i)
+        s = round(size_bytes /p, 8)
+        # s = round(size_bytes / p, 10)
+        # return "%s %s" % (s, size_name[i])
+        return s    
+    
+    def GetGeoCoordinates(self, TYPE ,Mat):
+        GPS_STATUS = 0
+        if TYPE == 'RG3':
+            try:
+                admin_georeference_coordinates = [Mat['Longitude_decimal_Xsens'][0][0] + Mat['Longitude_integer_Xsens'][0][0],Mat['Latitude_decimal_Xsens'][0][0]+Mat['Latitude_integer_Xsens'][0][0]]
+            except:
+                GPS_STATUS = 1
+                admin_georeference_coordinates = [0,0]      
+        
+        elif TYPE == 'CN7':
+            try:
+                # gnss_filter_positionlla_vector =pd.DataFrame(np.array(Mat['GNSS']['Filter_Positionlla_Vector']))
+                gnss_lat = Mat['GNSS']['GNSS_Latitude'][0][0]
+                gnss_long = Mat['GNSS']['GNSS_Longitude'][0][0]
+                admin_georeference_coordinates = [round(gnss_long,8),round(gnss_lat,8)]
+                # admin_georeference_coordinates=[round(float(gnss_filter_positionlla_vector[0].loc[0]['X'][0]),8),round(float(gnss_filter_positionlla_vector[0].loc[0]['Y'][0] ),8)]
+            except:
+                GPS_STATUS = 1
+                admin_georeference_coordinates = [0,0]      
+        return admin_georeference_coordinates, GPS_STATUS
+    
+    def GetTravelDistance(self, TYPE, Mat,admin_sampleTime):
+        CHASSIS_STATUS = 0
+        if TYPE == 'RG3':
+            try:
+                admin_travelDistance = round(((pd.DataFrame(Mat['WHL_SpdRLVal']) + pd.DataFrame(Mat['WHL_SpdRRVal']))/2).sum()[0]/3600*float(admin_sampleTime),8)
+            except:
+                CHASSIS_STATUS = 1
+                admin_travelDistance = 0
+        
+        elif TYPE == 'CN7':
+            chassis_data = pd.DataFrame(columns=['mean_spd'])
+            chassis_data['mean_spd'] = (Mat['Chassis']['LOG_BYTE0_WHLSPDRL'][0,:] + Mat['Chassis']['LOG_BYTE0_WHLSPDRR'][0,:])/2
+            try:
+                admin_travelDistance = round((chassis_data['mean_spd'].sum()/3600)*admin_sampleTime,8)
+            except:
+                CHASSIS_STATUS = 1
+                admin_travelDistance = 0
+        return admin_travelDistance, CHASSIS_STATUS
+
+    def GetLaneInfo(self, TYPE,Mat):
+        MOBILEYE_STATUS =0
+        lane_width = []
+        curvature = []
+        if TYPE == 'RG3':
+            try:
+                FR_CMR_Ln_LftDptDstVal = Mat['FR_CMR_Ln_LftDptDstVal']
+
+            except:
+                MOBILEYE_STATUS =1
+                lane_width = 0
+                curvature=0
+                return lane_width, curvature ,  MOBILEYE_STATUS
+            
+            FR_CMR_Ln_LftDptDstVal = Mat['FR_CMR_Ln_LftDptDstVal']
+            FR_CMR_Ln_LftCurveVal = Mat['FR_CMR_Ln_LftCurveVal']
+            FR_CMR_Ln_QualLvlLft01Sta = Mat['FR_CMR_Ln_QualLvlLft01Sta']
+            FR_CMR_Ln_QualLvlRt01Sta = Mat['FR_CMR_Ln_QualLvlRt01Sta']
+            FR_CMR_Ln_RtDptDstVal = Mat['FR_CMR_Ln_RtDptDstVal']
+            FR_CMR_Ln_RtCurveVal = Mat['FR_CMR_Ln_RtCurveVal']
+        
+        elif TYPE == "CN7" :
+            try:
+                FR_CMR_Ln_LftDptDstVal = Mat['Mobileye_Lane']['ME_Left_Lane_A_QualityLhME'][0,:]
+
+            except:
+                MOBILEYE_STATUS =1
+                lane_width = 0
+                curvature=0
+                return lane_width, curvature ,  MOBILEYE_STATUS
+            
+            FR_CMR_Ln_LftDptDstVal = Mat['Mobileye_Lane']['ME_Left_Lane_A_QualityLhME'][0,:]
+            FR_CMR_Ln_LftCurveVal = Mat['Mobileye_Lane']['ME_Right_Lane_A_QualityRhME'][0,:]
+            FR_CMR_Ln_QualLvlLft01Sta = Mat['Mobileye_Lane']['ME_Left_Lane_A_LaneMarkPositionC0LhME'][0,:]
+            FR_CMR_Ln_QualLvlRt01Sta = Mat['Mobileye_Lane']['ME_Right_Lane_A_LaneMarkPositionC0RhME'][0,:]
+            FR_CMR_Ln_RtDptDstVal = Mat['Mobileye_Lane']['ME_Left_Lane_A_LaneMarkModelAC2LhME'][0,:]
+            FR_CMR_Ln_RtCurveVal = Mat['Mobileye_Lane']['ME_Right_Lane_A_LaneMarkModelAC2RhME'][0,:]
+
+
+        for me_idx in range(np.size(FR_CMR_Ln_LftDptDstVal)):
+            if (FR_CMR_Ln_QualLvlLft01Sta[me_idx] ==3 and FR_CMR_Ln_QualLvlRt01Sta[me_idx] == 3):
+                lane_width.append(-FR_CMR_Ln_LftDptDstVal[me_idx]+FR_CMR_Ln_RtDptDstVal[me_idx])
+                curvature.append((FR_CMR_Ln_LftCurveVal[me_idx]+FR_CMR_Ln_RtCurveVal[me_idx])/2)
+            elif FR_CMR_Ln_QualLvlLft01Sta[me_idx] ==3:
+                lane_width.append(-FR_CMR_Ln_LftDptDstVal[me_idx]*2)
+                curvature.append(FR_CMR_Ln_LftCurveVal[me_idx])
+            elif FR_CMR_Ln_QualLvlRt01Sta[me_idx] == 3:
+                lane_width.append(FR_CMR_Ln_RtDptDstVal[me_idx]*2)
+                curvature.append(FR_CMR_Ln_RtCurveVal[me_idx])
+            elif (FR_CMR_Ln_QualLvlLft01Sta[me_idx] ==2 and FR_CMR_Ln_QualLvlRt01Sta[me_idx] == 2):
+                lane_width.append(-FR_CMR_Ln_LftDptDstVal[me_idx]+FR_CMR_Ln_RtDptDstVal[me_idx])
+                curvature.append((FR_CMR_Ln_LftCurveVal[me_idx]+FR_CMR_Ln_RtCurveVal[me_idx])/2)
+            elif FR_CMR_Ln_QualLvlLft01Sta[me_idx] ==2:
+                lane_width.append(-FR_CMR_Ln_LftDptDstVal[me_idx]*2)
+                curvature.append(FR_CMR_Ln_LftCurveVal[me_idx])
+            elif FR_CMR_Ln_QualLvlRt01Sta[me_idx] == 2:
+                lane_width.append(FR_CMR_Ln_RtDptDstVal[me_idx]*2)
+                curvature.append(FR_CMR_Ln_RtCurveVal[me_idx])
+        
+        return lane_width, curvature,MOBILEYE_STATUS   
+
+    def CalculateCurveEvent(self,TYPE ,Matsf):
+        CURVATURE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['PREPROCESSING'][0,0]['CURVATURE'][0,0][0,0]))
+        LEFT_LANE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['LEFT_LANE'][0,0][0,0]))
+        Front_Vision_Lane_sim = np.array((Matsf['SF_PP']['Front_Vision_Lane_sim'][0,0]))
+        sim_time = pd.DataFrame(np.array(Matsf['SF_PP']['sim_time']))[0][0]
+        # if TYPE == 'RG3':
+        #     CURVATURE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['PREPROCESSING'][0,0]['CURVATURE'][0,0][0,0]))
+        #     LEFT_LANE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['LEFT_LANE'][0,0][0,0]))
+        #     Front_Vision_Lane_sim = np.array((Matsf['SF_PP']['Front_Vision_Lane_sim'][0,0]))
+        #     sim_time = pd.DataFrame(np.array(Matsf['SF_PP']['sim_time']))[0][0]
+        
+        # elif TYPE == 'CN7':
+        #     CURVATURE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['PREPROCESSING'][0,0]['CURVATURE'][0,0][0,0]))
+        #     LEFT_LANE = (np.array(Matsf['SF_PP']['FRONT_VISION_LANE'][0,0]['LEFT_LANE'][0,0][0,0]))
+        #     Front_Vision_Lane_sim =  np.array((Matsf['SF_PP']['Front_Vision_Lane_sim'][0,0]))
+        #     sim_time = pd.DataFrame(np.array(Matsf['SF_PP']['sim_time']))[0][0]
+    # def cal_curve_event( sim_time ):
+        curve_event = np.zeros(np.size(sim_time))
+        time = np.size(curve_event) # 2399 정도 
+        curve_flag =np.zeros(np.size(sim_time))
+        curve_event_array = np.zeros([np.size(sim_time)])
+        
+        CURVATURE_REFERENCE_VALUE = 1/700
+        curve_event = pd.DataFrame(curve_event)
+        curve_flag =pd.DataFrame(curve_flag) 
+        curve_flag_size = 0
+        # curve_event.loc[1000] = 1 ## test index
+        # curve_event.loc[1001] = 0
+        # curve_event.loc[2000] = 1
+        # curve_event.loc[2001] = 0
+        # curve_event.loc[2100] = 1
+        # curve_event.loc[2101] = 0
+
+        for i in range(time):
+            if (np.abs(Front_Vision_Lane_sim[CURVATURE-1,LEFT_LANE-1,i]) >CURVATURE_REFERENCE_VALUE):
+                curve_event.iloc[i] = 1 # curve event occur
+                # print("curve event occur!\n")#test
+        
+        for k in range(time):
+            if (k == 0):
+                curve_flag.loc[0] = curve_event.loc[0]
+            
+            elif (k>0):     
+                if (float(curve_event.loc[k]) ==1 and float(curve_event.loc[k-1]) ==0): #Curve->Straight
+                    # print('Curve->Straight\n')
+                    curve_flag.loc[k] = 1
+                elif (float(curve_event.loc[k]) == 0 and float(curve_event.loc[k-1]) == 1):#Straight -> Curve
+                    # print('Straight -> Curve\n')
+                    curve_flag.loc[k] = 2
+        for i in range(time):
+            if float(curve_flag.loc[i] ) >  0 :                      
+                curve_flag_size +=1 
+        
+        return curve_flag , curve_flag_size
+
+    def get_scenery_event(self,curve_flag , scenery_event):
+        for i in range(np.size(curve_flag)):
+            tmp_roadGeometry =''
+            tmp_frameIndex = 0  
+            if float(curve_flag.loc[i]) == 1:
+                tmp_roadGeometry = 'CU'
+                tmp_frameIndex = i
+                scenery_event = pd.concat([scenery_event, pd.DataFrame([[tmp_frameIndex,tmp_roadGeometry]], columns=['frameIndex', 'roadGeometry'])], axis=0)
+            elif float(curve_flag.loc[i]) == 2:
+                tmp_roadGeometry = 'ST'
+                tmp_frameIndex = i     
+                scenery_event = pd.concat([scenery_event, pd.DataFrame([[tmp_frameIndex,tmp_roadGeometry]], columns=['frameIndex', 'roadGeometry'])], axis=0)  
+        return scenery_event
+    
+    def GetRoadName(self,Regi_xlsx, num):
+        dataNum = list(Regi_xlsx['dataNum'])
+        roadNamelist = list(Regi_xlsx['roadName'])
+        for i in range(0,np.size(dataNum),2):
+            if int(dataNum[i]) <= num+1 and int(dataNum[i+1]) >= num+1 :
+                roadName = roadNamelist[i]
+        return roadName 
+
+    def check_ODD(self, Regi_wrong_xlsx,num):
+        ODD_STATUS = 0
+        dataNum = list(Regi_wrong_xlsx['dataNum'])
+        roadNamelist = list(Regi_wrong_xlsx['Description'])
+        for i in range(0,np.size(dataNum)):
+            if int(dataNum[i] == num+1):
+                if int(roadNamelist[i]) == 7:
+                    ODD_STATUS = 1
+        return ODD_STATUS
+
+    def GetParticipants(self,mat_sf,index,labelDynamic,SENSORSIZE):
+        SF_PP_FUSION_TRACK_TRACKING_ID = int(np.array((mat_sf['SF_PP']['FUSION_TRACK'][0,0]['TRACKING'][0,0]['ID'][0,0]))) # 22
+        SF_PP_FUSION_TRACK_VEHICLE_RECOGNITION_RECOGNITION = int(np.array((mat_sf['SF_PP']['FUSION_TRACK'][0,0]['VEHICLE_RECOGNITION'][0,0]['RECOGNITION'][0,0]))) #50
+        ID = int(labelDynamic['ID'].iloc[index])
+        frameIndex = int(labelDynamic['FrameIndex'].iloc[index])
+        participants =[]
+        tmp_arr_ID = []
+        tmp_arr_recog = []
+        for i in range(64):
+            if frameIndex>= SENSORSIZE:
+                continue
+            try:
+                tmp_ID = get_Fusion_Track_Maneuver(mat_sf,SF_PP_FUSION_TRACK_TRACKING_ID,i,frameIndex)
+                tmp_recog = find_maneuver(get_Fusion_Track_Maneuver(mat_sf,SF_PP_FUSION_TRACK_VEHICLE_RECOGNITION_RECOGNITION,i,frameIndex))
+                if ((tmp_ID > 0) and (tmp_ID) != ID):
+                    if tmp_recog == 'FVL' or tmp_recog == 'FVI' or tmp_recog == 'FVR' or tmp_recog == 'AVL' or tmp_recog == 'AVR' or tmp_recog == 'RVL' or tmp_recog == 'RVI' or tmp_recog == 'RVR':
+                        tmp_arr_ID.append(tmp_ID)
+                        tmp_arr_recog.append(tmp_recog)
+            except:
+                continue
+        
+        size = np.size(tmp_arr_recog)
+        for index in range(size):
+            tmp = {
+                "recognition":str(tmp_arr_recog[index]),
+                "maneuver":"LK",
+                "category":2,
+                "participantID":int(tmp_arr_ID[index])
+                }      
+            participants.append(tmp)
+            
+        return participants
+
+    def GetParticipantObjframe(self, index , participants,labelDynamic):
+        participant_objframe = {
+            "frameIndex":int(labelDynamic['FrameIndex'].iloc[index]),
+            "ID":int(labelDynamic['ID'].iloc[index]),
+            "participants":participants
+        }
+        # print("test",index)
+        return participant_objframe
+
+    def GetCSS(self, admin_dataType,admin_sampleTime,admin_version,admin_projectName,directory,driver,admin_date,admin_travelTime,admin_fileSize,admin_georeference_type,admin_georeference_coordinates,admin_CMGT,\
+        admin_AESGT,admin_Stauts,admin_travelDistance,admin_annotationType,scenery,environment,dynamic,participant):
+
+        # version 0.4 
+        CSS = [{
+            "dataType":admin_dataType,
+            "sampleTime":admin_sampleTime,
+            "version":admin_version,
+            "projectName":admin_projectName, 
+            "directory":directory,
+            "driver":driver,
+            "date":admin_date,
+            "travelTime":admin_travelTime,
+            "fileSize": admin_fileSize,
+            "georeference":{
+                "type":admin_georeference_type,
+                "coordinates":admin_georeference_coordinates # 줄이 안맞음 []꼴로 나와야 하는데 줄이동이 생김 => 괜찮음 무시해도 되는 문제임
+            },
+            "parameter":{
+                "stationaryCondition":" ",
+                "trigger":" "
+            },
+            "CMGT":admin_CMGT,
+            "AESGT":admin_AESGT,
+            "Status":admin_Stauts,
+            "travelDistance":admin_travelDistance, #확인 필요
+            "annotationType":admin_annotationType,
+            "scenery":scenery,
+            "environment":environment,
+            "dynamic":dynamic,
+            "participant":participant,
+            }]    
+        
+        return CSS
+
+    def CheckCSSStatus(self, GPS_STATUS,CHASSIS_STATUS,MOBILEYE_STATUS,FRONT_RADAR_STATUS,CORNER_RADAR_STATUS,LIDAR_STATUS,ODD_STATUS,CSS_STATUS = []):
+        # CSS_STATUS =[]
+        if GPS_STATUS:
+            GPS_STATUS = 1
+            CSS_STATUS.append(GPS_STATUS)
+        if CHASSIS_STATUS:
+            CHASSIS_STATUS =2
+            CSS_STATUS.append(CHASSIS_STATUS)
+        if MOBILEYE_STATUS:
+            MOBILEYE_STATUS =3
+            CSS_STATUS.append(MOBILEYE_STATUS)
+        if FRONT_RADAR_STATUS:
+            FRONT_RADAR_STATUS = 4
+            CSS_STATUS.append(FRONT_RADAR_STATUS)
+        if CORNER_RADAR_STATUS:
+            CORNER_RADAR_STATUS = 5
+            CSS_STATUS.append(CORNER_RADAR_STATUS)                
+        if LIDAR_STATUS:
+            LIDAR_STATUS = 6
+            CSS_STATUS.append(LIDAR_STATUS)
+            
+        if ODD_STATUS:
+            ODD_STATUS = 7
+            CSS_STATUS.append(ODD_STATUS)    
+            
+        if np.size(CSS_STATUS) == 0:
+            CSS_STATUS.append(0)
+            
+        return CSS_STATUS
+
+    def get_driver_info(self):
+        driver = 10
+        
+        return driver
+
+
+    
 if __name__ == "__main__":
-    dir = ""
+    dir = r"\\192.168.75.251\Shares\FOT_Avante Data_1\Rosbag2Mat\CN7_030423"
     mkjson = MakeJson(dir)
     mkjson.AutoCuration()
